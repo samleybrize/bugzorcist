4a335a0c5be689b0ec8c174f8897c0911ae46e7f
Stephen Berquet <stephen.berquet@gmail.com>
2014-10-20 15:07:26 +0200

    license block

--- src/Bugzorcist/Exception/Renderer/ExceptionRendererHtml.php
+++ src/Bugzorcist/Exception/Renderer/ExceptionRendererHtml.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Bugzorcist.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Bugzorcist\Exception\Renderer;
 
 use Bugzorcist\VarDump\VarDumpHtml;
