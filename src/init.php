d2e6487fad1c24a5a66ce58fd313513fceed7155
Stephen Berquet <stephen.berquet@gmail.com>
2015-05-29 14:05:05 +0200

    fixed REQUEST_TIME_FLOAT on php<5.4

--- src/init.php
+++ src/init.php
@@ -1,6 +1,6 @@
 <?php
 
 // in PHP < 5.4, REQUEST_TIME_FLOAT does not exists
-if (array_key_exists("REQUEST_TIME_FLOAT", $_SERVER)) {
+if (!array_key_exists("REQUEST_TIME_FLOAT", $_SERVER)) {
     $_SERVER["REQUEST_TIME_FLOAT"] = microtime(true);
 }
